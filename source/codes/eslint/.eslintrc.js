8c2ddfd66cb210f5f16d54e6f45326aae9d48ff2
fortress-fight <dreamersfly@163.com>
2018-07-28 01:06:32 +0800

    publish ealint-使用

---
+++ source/codes/eslint/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    env: {
+        es6: true,
+        browser: true,
+        node: true
+    },
+    extends: ['standard'],
+    rules: {}
+}
