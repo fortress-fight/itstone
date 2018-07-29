36942024c4f28b49c7dc0d62fee670a490999fc7
fortress-fight <dreamersfly@163.com>
2018-07-29 23:39:14 +0800

    add new draft axios

--- source/codes/eslint/test.js
+++ source/codes/eslint/test.js
@@ -1,1 +1,15 @@
-var a = b;
+var obj = {
+    name: 1
+};
+var obj2 = {
+    name: 2
+};
+
+obj, obj2;
+
+function a(o) {}
+
+for (let index = 0; index < 1000; index++) {
+    const e = 1000;
+    a(e)
+}
